705fa38cb0f8e8b58c78362da10270c276f93892
curumim15 <80708544+Paulo23800@users.noreply.github.com>
2023-06-20 23:40:19 +0100

    Criação Projeto

---
+++ Server4emLinha/src/Controllers/PrimeiraTela.java
@@ -0,0 +1,11 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+
+public class PrimeiraTela {
+    public void ProximaPagina(ActionEvent actionEvent) {
+    }
+
+    public void Sair(ActionEvent actionEvent) {
+    }
+}
