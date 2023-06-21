0e00c95069afe0eeaa280563a44cc5e385a6852e
nedias <neca.dias28@gmail.com>
2023-06-21 11:07:15 +0100

    commit Primeira Tela -> Segunda Tela

--- Server4emLinha/src/Controllers/PrimeiraTela.java
+++ Server4emLinha/src/Controllers/PrimeiraTela.java
@@ -1,15 +1,35 @@
 package Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PrimeiraTela {
     public void ProximaPagina(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/SegundaTela.fxml"));
+            Parent root = loader.load();
 
+            Stage stage = new Stage();
+            stage.setTitle("Server Tela");
+            stage.setScene(new Scene(root, 800, 600));
+            stage.show();
+
+            // Fechar a janela atual da primeira tela
+            Stage currentStage = (Stage) ((javafx.scene.Node) actionEvent.getSource()).getScene().getWindow();
+            currentStage.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void Sair(ActionEvent actionEvent) {
         Stage stage = (Stage) ((javafx.scene.Node) actionEvent.getSource()).getScene().getWindow();
         stage.close();
+
     }
 }
--- Server4emLinha/src/Controllers/SegundaTela.java
+++ Server4emLinha/src/Controllers/SegundaTela.java
@@ -8,28 +8,28 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class PrimeiraTela {
+public class SegundaTela {
+    public void EscolherCor(ActionEvent actionEvent) {
+    }
+
     public void ProximaPagina(ActionEvent actionEvent) {
+    }
+
+    public void Voltar(ActionEvent actionEvent) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/SegundaTela.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/PrimeiraTela.fxml"));
             Parent root = loader.load();
 
             Stage stage = new Stage();
-            stage.setTitle("Cliente Tela");
+            stage.setTitle("Server Tela");
             stage.setScene(new Scene(root, 800, 600));
             stage.show();
 
-            // Fechar a janela atual da primeira tela
+            // Fechar a janela atual da segunda tela
             Stage currentStage = (Stage) ((javafx.scene.Node) actionEvent.getSource()).getScene().getWindow();
             currentStage.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public void Sair(ActionEvent actionEvent) {
-        Stage stage = (Stage) ((javafx.scene.Node) actionEvent.getSource()).getScene().getWindow();
-        stage.close();
-
     }
 }
