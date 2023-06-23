d2d7781b8100ff84b14df2a23339ce1a8ad2c2cd
nedias <neca.dias28@gmail.com>
2023-06-23 16:48:53 +0100

    commit Segunda Tela -> Terceira Tela

--- Client4emLinha/src/Controllers/SegundaTela.java
+++ Client4emLinha/src/Controllers/SegundaTela.java
@@ -13,6 +13,21 @@ public class SegundaTela {
     }
 
     public void ProximaPagina(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/TerceiraTela.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Server");
+            stage.setScene(new Scene(root, 800, 600));
+            stage.show();
+
+            // Fechar a janela atual da segunda tela
+            Stage currentStage = (Stage) ((javafx.scene.Node) actionEvent.getSource()).getScene().getWindow();
+            currentStage.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void Voltar(ActionEvent actionEvent) {
