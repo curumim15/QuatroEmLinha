1992c4c6a9dd3c49499acb3e664f5d33b92a90ec
Paulo23800 <pjorgecarvalho@ipvc.pt>
2023-06-24 19:58:18 +0100

    Commit Server

--- Client4emLinha/src/Client/Client.java
+++ Client4emLinha/src/Client/Client.java
@@ -1,4 +1,28 @@
 package Client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void conectarAoServidor(String ip) {
+        Thread serverThread = new Thread(() -> {
+            try {
+                socket = new Socket(ip, 8888);
+                out = new PrintWriter(socket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        serverThread.start();
+
+    }
 }
