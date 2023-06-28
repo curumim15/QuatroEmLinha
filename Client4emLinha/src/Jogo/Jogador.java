e78640303ba47cfb88822f70b9e7d473ca033ea8
Paulo23800 <pjorgecarvalho@ipvc.pt>
2023-06-28 18:47:35 +0100

    Commit Server/Client

--- Client4emLinha/src/Jogo/Jogador.java
+++ Client4emLinha/src/Jogo/Jogador.java
@@ -1,4 +1,24 @@
 package Jogo;
 
 public class Jogador {
+    private String nome;
+    private String cor;
+
+    // Implemente os demais atributos e métodos necessários
+
+    public String getName() {
+        return nome;
+    }
+
+    public void setName(String name) {
+        this.nome = name;
+    }
+
+    public String getColor() {
+        return cor;
+    }
+
+    public void setColor(String color) {
+        this.cor = color;
+    }
 }
