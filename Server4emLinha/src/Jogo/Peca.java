da1e193e1744f8da13b25f3efc0918ce1136bea8
Paulo23800 <pjorgecarvalho@ipvc.pt>
2023-06-30 20:38:29 +0100

    Commit Jogo Peca

--- Server4emLinha/src/Jogo/Peca.java
+++ Server4emLinha/src/Jogo/Peca.java
@@ -1,4 +1,27 @@
 package Jogo;
 
 public class Peca {
+    private Jogador jogador;
+    private String cor;
+
+
+    public Jogador getJogador() {
+        return jogador;
+    }
+
+    public void setJogador(Jogador jogador) {
+        this.jogador = jogador;
+    }
+
+    public boolean isEmpty() {
+        return jogador == null;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor){
+        this.cor = cor;
+    }
 }
