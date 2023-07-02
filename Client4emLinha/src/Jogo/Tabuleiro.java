4fb8e2a0dbe35ba3676f923cb1e33d700d365742
Paulo23800 <pjorgecarvalho@ipvc.pt>
2023-07-02 21:58:32 +0100

    Commit Jogo Tabuleiro

--- Client4emLinha/src/Jogo/Tabuleiro.java
+++ Client4emLinha/src/Jogo/Tabuleiro.java
@@ -1,4 +1,22 @@
 package Jogo;
 
 public class Tabuleiro {
+    private Peca[][] pecas;
+    private int rows;
+    private int columns;
+
+    public Tabuleiro(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        pecas = new Peca[rows][columns];
+        initializePecas();
+    }
+
+    private void initializePecas() {
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < columns; col++) {
+                pecas[row][col] = new Peca();
+            }
+        }
+    }
 }
